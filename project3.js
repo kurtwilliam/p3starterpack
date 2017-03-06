64802134e57bbeaafe0bf98165d84f36b7e6f394
Kurt Schneider <k.w.schneid@gmail.com>
2017-03-06 17:37:42 -0500

    Initial commit AHHgit status

---
+++ project3.js
@@ -0,0 +1,13 @@
+
+	var Questionlist = [
+
+	q1AnswerKey = {
+		qtitle : "what hat r u",
+		AnswerKey:{
+			answer1 : 'img1',
+			answer2 : 'img2',
+			answer3 : 'img3',
+			answer4 : 'img4'
+			}
+		}
+	]
---
+++ script.js
@@ -0,0 +1,12 @@
+	// var Questionlist = [
+
+	var q1AnswerKey : {
+		qtitle : "what hat r u",
+		AnswerKey:{
+			answer1 : 'img1',
+			answer2 : 'img2',
+			answer3 : 'img3',
+			answer4 : 'img4'
+			}
+		}
+	// ]
